645585a924c16273907699958ba698b9de8be7b0
haxingren <tanjingyuan17@gmail.com>
2024-07-07 21:08:15 +0800

    Add files via upload

---
+++ 练习 以ASCII码输出其对应的字符常量.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	//输出ASCII码79 88 94 93 60 08对应的字符常量
+	printf("%c\n", 78);
+	int arr[] = { 79, 88, 94, 60, 77 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	while (i <= sz)
+	{
+		printf("%c ", arr[i]);
+		i++;
+	}
+	return 0;
+}
