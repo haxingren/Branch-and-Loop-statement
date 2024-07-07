2fe0be120575d436c72323acc25b2ff627e6119a
haxingren <tanjingyuan17@gmail.com>
2024-07-07 21:37:43 +0800

    Add files via upload

---
+++ 练习 print的返回值.c
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+//printf的返回值为输出的字符串的字符个数
+{
+	int n = printf("Hello world!");//Hello world!算上字母，空格，符号加起来共12个字符，所以输出12
+	printf("\n%d\n", n);//前面加个\n就可以先换行再输出
+	return 0;
+}
