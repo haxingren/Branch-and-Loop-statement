e8c3ec534a1f813b310a76536c9c249270f97e5b
haxingren <tanjingyuan17@gmail.com>
2024-07-10 20:14:11 +0800

    Add files via upload

---
+++ 1.23控制语句之 循环do while语句.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	////do while循环
+	int a = 0;
+	do
+	{
+		printf("%d ", a);
+		a++;
+	} while (a <= 10);
+	//因为是先运行，后判断，所以do while 循环至少要进行一次
+	//有局限性，所以较少使用
+
+}
