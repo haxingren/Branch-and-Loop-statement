3ae69acd7f1953daf9bfb0a22dbc5a401b2fe648
haxingren <tanjingyuan17@gmail.com>
2024-07-11 21:42:26 +0800

    Add files via upload

---
+++ 循环练习2 算1~10的阶乘总和.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	//int a = 0;
+	//int b = 2;
+	//int ret = 1;
+	//int sum = 0;
+	//for (b = 1; b <= 10; b++)
+	//{
+	//	for (a = 1, ret = 1; a <= b; a++)
+	//	{
+	//		ret = ret * a;
+	//	}
+	//	sum = sum + ret;
+	//}
+	//printf("%d\n", sum);//这个太复杂
+
+	int n = 0;
+	int ret = 1;
+	int sum = 0;
+	for (n = 1; n <= 10; n++)
+	{
+		ret = ret * n;
+		sum = sum + ret;
+	}
+	printf("%d\n", sum);//这个就简洁许多
+	return 0;
+}
