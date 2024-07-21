397f9d099cc24ae8bd9f5a9a269a0d6f8e2866ff
haxingren <tanjingyuan17@gmail.com>
2024-07-21 16:54:07 +0800

    Add files via upload

---
+++ 循环练习6 打印九乘九乘法表.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	for (x = 1; x <= 9; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			int ret = x * y;
+			//printf("%d * %d = %2d ", x, y, ret);//右对齐
+			printf("%d * %d = %-2d ", x, y, ret);//左对齐
+			//if (x == y)
+			//	break;
+		}
+		printf("\n");
+	}
+	return 0;
+}
